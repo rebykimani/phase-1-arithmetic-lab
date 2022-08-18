51739dd1bee00dfeedadc1d99bad5f36a6a2d24f
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-19 01:09:32 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
+const random = (1, 2, 4, 5);
+Math.random();
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+const max = (12, 5, 20);
+Math.max(); 
